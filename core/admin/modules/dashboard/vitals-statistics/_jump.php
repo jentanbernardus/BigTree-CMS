de82543f1d732824281c44f0559ee348d0033baa
Fastspot <fastspot@Fastspots-Mac-Pro.local>
2012-03-07 19:04:03 -0500

    what

---
+++ core/admin/modules/dashboard/vitals-statistics/_jump.php
@@ -0,0 +1,9 @@
+<div class="jump_group">
+	<span class="icon"></span>
+	<div class="dropdown">
+		<strong>Vitals &amp; Statistics</strong>
+		<a href="<?=$aroot?>dashboard/vitals-statistics/analytics/">Analytics</a>
+		<a href="<?=$aroot?>dashboard/vitals-statistics/404/">404 Report</a>
+		<a href="<?=$aroot?>dashboard/vitals-statistics/integrity/">Site Inegrity</a>
+	</div>
+</div>
