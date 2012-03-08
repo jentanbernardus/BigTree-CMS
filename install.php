d3f6da03e0867e4ec8593dd07e512be5df57756c
Tim Buckingham <tim@fastspot.com>
2012-03-08 13:38:37 -0500

    Removed foreach vulnerability in installer.  Made installer use full <?php tags.

--- install.php
+++ install.php
@@ -1,55 +1,50 @@
-<?
+<?php
 	//!Server Parameters
 	$warnings = array();
-	if (!extension_loaded('json'))
+	if (!extension_loaded('json')) {
 		$warnings[] = "JSON Extension is missing (this could affect API and Foundry usage).";
-	if (!extension_loaded("mysql"))
+	}
+	if (!extension_loaded("mysql")) {
 		$warnings[] = "MySQL Extension is missing (this is a FATAL ERROR).";
-	if (get_magic_quotes_gpc())
+	}
+	if (get_magic_quotes_gpc()) {
 		$warnings[] = "magic_quotes_gpc is on. BigTree will attempt to override this at runtime but it is advised that you turn it off in php.ini.";
-	if (!ini_get('file_uploads'))
+	}
+	if (!ini_get('file_uploads')) {
 		$warnings[] = "PHP does not have file uploads enabled. This will severely limit BigTree's functionality.";
-	if (!ini_get('short_open_tag'))
+	}
+	if (!ini_get('short_open_tag')) {
 		$warnings[] = "PHP does not currently allow short_open_tags. BigTree will attempt to override this at runtime but you may need to enable it in php.ini manually.";
-	if (!extension_loaded('gd') && !extension_loaded('imagick'))
+	}
+	if (!extension_loaded('gd') && !extension_loaded('imagick')) {
 		$warnings[] = "PHP does not have GD or ImageMagick enabled. This will severely limit your ability to do anything with images in BigTree.";
-	if (!function_exists("ftp_connect"))
-		$warnings[] = "PHP does not have FTP support installed. Dev->Live sync will not work without FTP support.";
-	if (intval(ini_get('upload_max_filesize')) < 4)
+	}
+	if (intval(ini_get('upload_max_filesize')) < 4) {
 		$warnings[] = "Max upload filesize is currently less than 4MB. 8MB or higher is recommended.";
-	if (intval(ini_get('upload_max_filesize')) < 4)
+	}
+	if (intval(ini_get('upload_max_filesize')) < 4) {
 		$warnings[] = "Max upload filesize (upload_max_filesize in php.ini) is currently less than 4MB. 8MB or higher is recommended.";
-	if (intval(ini_get('post_max_size')) < 4)
+	}
+	if (intval(ini_get('post_max_size')) < 4) {
 		$warnings[] = "Max POST size (post_max_size in php.ini) is currently less than 4MB. 8MB or higher is recommended.";
-	if (intval(ini_get("memory_limit")) < 32)
+	}
+	if (intval(ini_get("memory_limit")) < 32) {
 		$warnings[] = "PHP's memory limit is currently under 32MB. BigTree recommends at least 32MB of memory be available to PHP.";
+	}
 	$apache_modules = apache_get_modules();
-	if (in_array('mod_rewrite', $apache_modules) === false)
+	if (in_array('mod_rewrite', $apache_modules) === false) {
 		$warnings[] = "BigTree requires Apache to have mod_rewrite installed (this is a FATAL ERROR).";
+	}
 
-?><!doctype html> 
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>	<html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>	<html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>	<html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>Install BigTree</title>
-		<link rel="stylesheet" href="core/admin/css/install.css" type="text/css" media="all" />
-		<script type="text/javascript" src="core/admin/js/lib.js"></script>
-		<script type="text/javascript" src="core/admin/js/install.js"></script>
-	</head>
-	<body class="install">
-		<div class="install_wrapper">
-<?php
-
+	// Clean all post variables up.
 	foreach ($_POST as $key => $val) {
-		$$key = $val;
+		if (substr($key,0,1) != "_") {
+			$$key = $val;
+		}
 	}
 	
 	$success = false;
+	$installed = false;
 
 	if (count($_POST) && !($db && $host && $user && $password && $cms_user && $cms_pass)) {
 		$error = "Errors found! Please fix the highlighted fields before submitting.";
@@ -65,8 +60,9 @@
 			$error = "Could not connect to database.";
 		} else {
 			$select = mysql_select_db($db, $con);
-			if (!$select)
+			if (!$select) {
 				$error = "Could not select database &ldquo;$db&rdquo;.";
+			}
 		}
 	}
 	
@@ -169,7 +165,7 @@
 		dwrite("templates/layouts/");
 		dtouch("templates/layouts/_header.php");
 		dtouch("templates/layouts/default.php",'<? include "_header.php" ?>
-<?=$content?>
+<?php echo $content?>
 <? include "_footer.php" ?>');
 		dtouch("templates/layouts/_footer.php");
 		dwrite("templates/modules/");
@@ -177,8 +173,8 @@
 		dtouch("templates/pages/_404.php");
 		dtouch("templates/pages/_home.php");
 		dtouch("templates/pages/_sitemap.php");
-		dtouch("templates/pages/content.php",'<h1><?=$page_header?></h1>
-<?=$page_content?>');
+		dtouch("templates/pages/content.php",'<h1><?php echo $page_header?></h1>
+<?php echo $page_content?>');
 		dwrite("templates/callouts/");
 		dwrite("templates/objects/");
 		dwrite("templates/objects/containers/");
@@ -315,18 +311,39 @@ php_flag magic_quotes_gpc Off');
 	RewriteRule    ^$    site/    [L]
 	RewriteRule    (.*) site/$1    [L]
 </IfModule>');
+
+		$installed = true;
+	}
+	
+	// Set localhost as the default MySQL host
+	if (!$host) {
+		$host = "localhost";
+	}
 ?>
+<!doctype html> 
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>	<html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>	<html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>	<html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Install BigTree</title>
+		<link rel="stylesheet" href="core/admin/css/install.css" type="text/css" media="all" />
+		<script type="text/javascript" src="core/admin/js/lib.js"></script>
+		<script type="text/javascript" src="core/admin/js/install.js"></script>
+	</head>
+	<body class="install">
+		<div class="install_wrapper">
+			<?php if ($installed) { ?>
 			<h1>BigTree Installed</h1>
 			<form method="post" action="" class="module">
 				<fieldset class="clear">
 					<p>Thanks for using BigTree CMS. The installation was successful. <a href="admin/">Ready to hit the admin?</a></p><br /><br />
 				</fieldset>
 			</form>
-<?
-	} else {
-		if (!$host)
-			$host = "localhost";
-?>
+			<?php } else { ?>
 			<h1>Install BigTree</h1>
 			<form method="post" action="" class="module">
 				<h2 class="getting_started"><span></span>Getting Started</h2>
@@ -334,15 +351,22 @@ php_flag magic_quotes_gpc Off');
 					<p>Welcome to the BigTree installer. If you have not done so already, please make the current working directory writable and create a MySQL database for your new BigTree powered site.</p>
 					<br />
 				</fieldset>
-				<? if (count($warnings)) { ?>
-				<br />
-				<? foreach ($warnings as $warning) { ?>
-				<p class="warning_message clear"><?=$warning?></p>
-				<? } ?>
-				<? } ?>
-				<? if ($error) { ?>
-				<p class="error_message clear"><?=$error?></p>
-				<? } ?>
+				<?php
+					if (count($warnings)) {
+						echo '<br />';
+						foreach ($warnings as $warning) {
+				?>
+				<p class="warning_message clear"><?php echo $warning?></p>
+				<?php
+						}
+					}
+					
+					if ($error) {
+				?>
+				<p class="error_message clear"><?php echo $error?></p>
+				<?php
+					}
+				?>
 				<hr />
 				
 				<h2 class="database"><span></span>Database Properties</h2>
@@ -350,30 +374,30 @@ php_flag magic_quotes_gpc Off');
 					<p>Enter your MySQL database information below.</p>
 					<br />
 				</fieldset>
-				<fieldset class="left<? if (count($_POST) && !$host) { ?> form_error<? } ?>">
+				<fieldset class="left<?php if (count($_POST) && !$host) { ?> form_error<?php } ?>">
 					<label>Hostname</label>
-					<input class="text" type="text" id="db_host" name="host" value="<?=htmlspecialchars($host)?>" tabindex="1" />
+					<input class="text" type="text" id="db_host" name="host" value="<?php echo htmlspecialchars($host) ?>" tabindex="1" />
 				</fieldset>
-				<fieldset class="right<? if (count($_POST) && !$db) { ?> form_error<? } ?>">
+				<fieldset class="right<?php if (count($_POST) && !$db) { ?> form_error<?php } ?>">
 					<label>Database</label>
-					<input class="text" type="text" id="db_name" name="db" value="<?=htmlspecialchars($db)?>" tabindex="2" />
+					<input class="text" type="text" id="db_name" name="db" value="<?php echo htmlspecialchars($db) ?>" tabindex="2" />
 				</fieldset>
 				<br class="clear" /><br />
-				<fieldset class="left<? if (count($_POST) && !$user) { ?> form_error<? } ?>">
+				<fieldset class="left<?php if (count($_POST) && !$user) { ?> form_error<?php } ?>">
 					<label>Username</label>
-					<input class="text" type="text" id="db_user" name="user" value="<?=htmlspecialchars($user)?>" tabindex="3" />
+					<input class="text" type="text" id="db_user" name="user" value="<?php echo htmlspecialchars($user) ?>" tabindex="3" />
 				</fieldset>
-				<fieldset class="right<? if (count($_POST) && !$password) { ?> form_error<? } ?>">
+				<fieldset class="right<?php if (count($_POST) && !$password) { ?> form_error<?php } ?>">
 					<label>Password</label>
-					<input class="text" type="password" id="db_pass" name="password" value="<?=htmlspecialchars($password)?>" tabindex="4" />
+					<input class="text" type="password" id="db_pass" name="password" value="<?php echo htmlspecialchars($password) ?>" tabindex="4" />
 				</fieldset>
 				<fieldset>
 					<br />
-					<input type="checkbox" class="checkbox" name="loadbalanced" id="loadbalanced""<? if ($loadbalanced) { ?> checked="checked"<? } ?> tabindex="5" />
+					<input type="checkbox" class="checkbox" name="loadbalanced" id="loadbalanced"<?php if ($loadbalanced) { ?> checked="checked"<?php } ?> tabindex="5" />
 					<label class="for_checkbox">Load Balanced MySQL</label>
 				</fieldset>
 				
-				<div id="loadbalanced_settings"<? if (!$loadbalanced) { ?> style="display: none;"<? } ?>>
+				<div id="loadbalanced_settings"<?php if (!$loadbalanced) { ?> style="display: none;"<?php } ?>>
 					<br class="clear" />
 					<hr />
 					
@@ -382,22 +406,22 @@ php_flag magic_quotes_gpc Off');
 						<p>If you are hosting a load balanced setup with multiple MySQL servers, enter the master write server information below.</p>
 						<br />
 					</fieldset>
-					<fieldset class="left<? if (count($_POST) && !$write_host) { ?> form_error<? } ?>">
+					<fieldset class="left<?php if (count($_POST) && !$write_host) { ?> form_error<?php } ?>">
 						<label>Hostname</label>
-						<input class="text" type="text" id="db_write_host" name="write_host" value="<?=htmlspecialchars($host)?>" tabindex="6" />
+						<input class="text" type="text" id="db_write_host" name="write_host" value="<?php echo htmlspecialchars($host) ?>" tabindex="6" />
 					</fieldset>
-					<fieldset class="right<? if (count($_POST) && !$write_db) { ?> form_error<? } ?>">
+					<fieldset class="right<?php if (count($_POST) && !$write_db) { ?> form_error<?php } ?>">
 						<label>Database</label>
-						<input class="text" type="text" id="db_write_name" name="write_db" value="<?=htmlspecialchars($db)?>" tabindex="7" />
+						<input class="text" type="text" id="db_write_name" name="write_db" value="<?php echo htmlspecialchars($db) ?>" tabindex="7" />
 					</fieldset>
 					<br class="clear" /><br />
-					<fieldset class="left<? if (count($_POST) && !$write_user) { ?> form_error<? } ?>">
+					<fieldset class="left<?php if (count($_POST) && !$write_user) { ?> form_error<?php } ?>">
 						<label>Username</label>
-						<input class="text" type="text" id="db_write_user" name="write_user" value="<?=htmlspecialchars($user)?>" tabindex="8" />
+						<input class="text" type="text" id="db_write_user" name="write_user" value="<?php echo htmlspecialchars($user) ?>" tabindex="8" />
 					</fieldset>
-					<fieldset class="right<? if (count($_POST) && !$write_password) { ?> form_error<? } ?>">
+					<fieldset class="right<?php if (count($_POST) && !$write_password) { ?> form_error<?php } ?>">
 						<label>Password</label>
-						<input class="text" type="password" id="db_write_pass" name="write_password" value="<?=htmlspecialchars($password)?>" tabindex="9" />
+						<input class="text" type="password" id="db_write_pass" name="write_password" value="<?php echo htmlspecialchars($password) ?>" tabindex="9" />
 					</fieldset>
 					<br class="clear" />
 				</div>
@@ -410,13 +434,13 @@ php_flag magic_quotes_gpc Off');
 					<p>Customize your site's security settings below.</p>
 					<br />
 				</fieldset>
-				<fieldset class="left<? if (count($_POST) && !$settings_key) { ?> form_error<? } ?>">
+				<fieldset class="left<?php if (count($_POST) && !$settings_key) { ?> form_error<?php } ?>">
 					<label>Settings Encryption Key</label>
-					<input class="text" type="text" name="settings_key" id="settings_key" value="<?=htmlspecialchars($settings_key)?>" tabindex="10" />
+					<input class="text" type="text" name="settings_key" id="settings_key" value="<?php echo htmlspecialchars($settings_key) ?>" tabindex="10" />
 				</fieldset>
 				<fieldset class="clear">
 					<br />
-					<input type="checkbox" class="checkbox" name="force_secure_login" id="force_secure_login"<? if ($force_secure_login) { ?> checked="checked"<? } ?> tabindex="11" />
+					<input type="checkbox" class="checkbox" name="force_secure_login" id="force_secure_login"<?php if ($force_secure_login) { ?> checked="checked"<?php } ?> tabindex="11" />
 					<label class="for_checkbox">Force HTTPS Logins</label>
 				</fieldset>
 				
@@ -428,13 +452,13 @@ php_flag magic_quotes_gpc Off');
 					<p>Create the default account your administration area.</p>
 					<br />
 				</fieldset>
-				<fieldset class="left<? if (count($_POST) && !$cms_user) { ?> form_error<? } ?>">
+				<fieldset class="left<?php if (count($_POST) && !$cms_user) { ?> form_error<?php } ?>">
 					<label>Email Address</label>
-					<input class="text" type="text" id="cms_user" name="cms_user" value="<?=htmlspecialchars($cms_user)?>" tabindex="12" />
+					<input class="text" type="text" id="cms_user" name="cms_user" value="<?php echo htmlspecialchars($cms_user) ?>" tabindex="12" />
 				</fieldset>
-				<fieldset class="right<? if (count($_POST) && !$cms_pass) { ?> form_error<? } ?>">
+				<fieldset class="right<?php if (count($_POST) && !$cms_pass) { ?> form_error<?php } ?>">
 					<label>Password</label>
-					<input class="text" type="password" id="cms_pass" name="cms_pass" value="<?=htmlspecialchars($cms_pass)?>" tabindex="13" />
+					<input class="text" type="password" id="cms_pass" name="cms_pass" value="<?php echo htmlspecialchars($cms_pass) ?>" tabindex="13" />
 				</fieldset>
 				
 				<br class="clear" />
@@ -447,7 +471,7 @@ php_flag magic_quotes_gpc Off');
 				</fieldset>
 				<fieldset class="clear">
 					<br />
-					<input type="checkbox" class="checkbox" name="install_example_site" id="install_example_site"<? if ($install_example_site) { ?> checked="checked"<? } ?> tabindex="14" />
+					<input type="checkbox" class="checkbox" name="install_example_site" id="install_example_site"<?php if ($install_example_site) { ?> checked="checked"<?php } ?> tabindex="14" />
 					<label class="for_checkbox">Install Example Site</label>
 				</fieldset>
 				
@@ -457,11 +481,9 @@ php_flag magic_quotes_gpc Off');
 					<input type="submit" class="button blue" value="Install Now" tabindex="15" />
 				</fieldset>
 			</form>
-<?php
-	}
-?>
+			<?php } ?>
 			<a href="http://www.bigtreecms.com" class="install_logo" target="_blank">BigTree</a>
-			<a href="http://www.fastspot.com" class="install_copyright" target="_blank">&copy; <?=date("Y")?> Fastspot</a>
+			<a href="http://www.fastspot.com" class="install_copyright" target="_blank">&copy; <?php echo date("Y") ?> Fastspot</a>
 		</div>
 	</body>
 </html>
