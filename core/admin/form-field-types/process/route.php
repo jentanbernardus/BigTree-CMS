7aefc9b3d65b1350f1a2fd4ac4e7c8204f171613
Fastspot <info@fastspot.com>
2012-03-12 13:27:56 -0400

    Ensure no HTML tags exist in a generated route.

--- core/admin/form-field-types/process/route.php
+++ core/admin/form-field-types/process/route.php
@@ -1,9 +1,9 @@
 <?
 	if ($data[$key] == "generate") {
 		if ($options["not_unique"]) {
-			$value = $cms->urlify($data[$options["source"]]);		
+			$value = $cms->urlify(strip_tags($data[$options["source"]]));
 		} else {
-			$oroute = $cms->urlify($data[$options["source"]]);
+			$oroute = $cms->urlify(strip_tags($data[$options["source"]]));
 			$value = $oroute;
 			$x = 2;
 			while (sqlrows(sqlquery("SELECT * FROM ".$form["table"]." WHERE `$key` = '".mysql_real_escape_string($value)."' AND id != '".$_POST["id"]."'"))) {
