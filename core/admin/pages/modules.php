2ebe3fbd090e05c8305c5f8a74caa6a9a00886c2
Ben Plum <benjaminplum@gmail.com>
2012-03-25 15:29:35 -0400

    Updates to module default screen - shows message when module groups exists but no any actually modules.

--- core/admin/pages/modules.php
+++ core/admin/pages/modules.php
@@ -32,6 +32,7 @@
 ?>
 <h1><span class="modules"></span>Modules</h1>
 <?
+		$module_count = 0;
 		$groups = $admin->getModuleGroups();
 		foreach ($groups as $group) {
 			$modules = $admin->getModulesByGroup($group["id"]);
@@ -40,14 +41,19 @@
 <div class="table">
 	<summary><h2><?=$group["name"]?></h2></summary>
 	<section class="modules">
-		<? foreach ($modules as $module) { ?>
+		<? 
+			foreach ($modules as $module) { 
+		?>
 		<p class="module">
 			<? if ($admin->moduleActionExists($module["id"],"add")) { ?>
 			<a href="<?=$admin_root?><?=$module["route"]?>/add/" class="add"><span class="icon_small icon_small_add"></span></a>
 			<? } ?>
 			<a class="module_name" href="<?=$admin_root?><?=$module["route"]?>/"><?=$module["name"]?></a>
 		</p>
-		<? } ?>
+		<? 
+				$module_count++;
+			} 
+		?>
 	</section>
 </div>
 <?
@@ -60,20 +66,25 @@
 <div class="table">
 	<summary><h2>Ungrouped</h2></summary>
 	<section class="modules">
-		<? foreach ($misc as $module) { ?>
+		<? 
+			foreach ($misc as $module) { 
+		?>
 		<p class="module">
 			<? if ($admin->moduleActionExists($module["id"],"add")) { ?>
 			<a href="<?=$admin_root?><?=$module["route"]?>/add/" class="add"><span class="icon_small icon_small_add"></span></a>
 			<? } ?>
 			<a class="module_name" href="<?=$admin_root?><?=$module["route"]?>/"><?=$module["name"]?></a>
 		</p>
-		<? } ?>
+		<? 
+				$module_count++;
+			} 
+		?>
 	</section>
 </div>
 <?
    		}
-	
-		if (!count($groups) && !count($misc)) {
+   		
+		if ($module_count < 1) {
 ?>
 <div class="table">
 	<summary><h2>Ungrouped</h2></summary>
